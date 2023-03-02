a546730836bf45de9894a2da82173fe2f050c521
jayde225 <jayde225@users.noreply.github.com>
2023-03-02 15:17:41 -0600

    Convert Node files to TypeScript
    Converted Node files to TypeScript files

--- src/ManageData.js
+++ src/ManageData.js
@@ -1,26 +1,22 @@
-const http = require('http');
-const fs = require("fs"); // Use for reading files
-const data = require('./database/sampleData.json'); // JSON file to use
-
+var http = require('http');
+var fs = require("fs"); // Use for reading files
+var data = require('./database/sampleData.json'); // JSON file to use
 // *** WEB SERVER SET UP ***
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+var hostname = '127.0.0.1';
+var port = 3000;
+var server = http.createServer(function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, function () {
+    console.log("Server running at http://".concat(hostname, ":").concat(port, "/"));
 });
-
 // *** OTHER BACKEND ***
-fs.readFile("./database/sampleData.json", function(err, data) {
-  if (err) throw err; // Check for errors
- 
-  // Convert to JS object and print
-  const tickets = JSON.parse(data);
-  console.log(tickets); // Print ticket information 
+fs.readFile("./database/sampleData.json", function (err, data) {
+    if (err)
+        throw err; // Check for errors
+    // Convert to JS object and print
+    var tickets = JSON.parse(data);
+    console.log(tickets); // Print ticket information 
 });
---
+++ src/ManageData.ts
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require("fs"); // Use for reading files
+const data = require('./database/sampleData.json'); // JSON file to use
+
+// *** WEB SERVER SET UP ***
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// *** OTHER BACKEND ***
+fs.readFile("./database/sampleData.json", function(err, data) {
+  if (err) throw err; // Check for errors
+ 
+  // Convert to JS object and print
+  const tickets = JSON.parse(data);
+  console.log(tickets); // Print ticket information 
+});
